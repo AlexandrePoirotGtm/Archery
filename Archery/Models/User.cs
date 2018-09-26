762de95e8dee2432e477513d09adc1b1110b090b
sd <dfdd@kdkjd.com>
2018-09-26 12:19:03 +0200

    Ajout DataType et htmlHelper pour form

--- Archery/Models/User.cs
+++ Archery/Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,11 +9,15 @@ namespace Archery.Models
     public abstract class User
     {
         public int ID { get; set; }
+        [DataType(DataType.EmailAddress)]
         public string Mail { get; set; }
+        [DataType(DataType.Password)]
         public string Password { get; set; }
+        [DataType(DataType.Password)]
         public string ConfirmedPassword { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        [DataType(DataType.Date)]
         public DateTime? BirthDate { get; set; }
     }
 }
